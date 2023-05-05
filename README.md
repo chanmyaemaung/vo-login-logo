f99ea8aa02ce64bacb910299b4faeee1bdfc6aba
Conor <conor@visibleone.com>
2023-05-05 11:16:19 +0800

    status: Implemented Upload/Image featured. :white_check_mark:

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 The VO Login Logo plugin enables you to modify the WordPress login logo using a media file or a remote URL from the settings page.
 
+## Plugin Settings Preview
+
+![Plugin Settings Preview](https://i.imgur.com/NERQOP1.png)
+
 ## Features
 
 - Replace the default WordPress login logo with a custom logo.
 - Choose between uploading a logo from your media library or using a remote URL.
 - Simple and user-friendly interface.
 - Compatible with all WordPress versions tested up to 6.2.
-- Developed by Chen Lay (<a href="https://www.facebook.com/mynameischen">Conor</a>).
+- Developed by Chen Lay ([Conor](https://www.facebook.com/mynameischen)).
 - Open source project - anyone can use, download, modify, contribute to, and distribute the plugin free of charge.
 
 ## Installation
@@ -34,8 +38,6 @@ If you encounter any issues while using the VO Login Logo plugin, don't hesitate
 ## Contribute
 
 Anyone in the world who wants to help make the VO Login Logo app better is welcome to do so. Visit our GitHub project at https://github.com/chanmyaemaung/vo-login-logo if you want to help with the plugin.
-
-
 
 ## License
 
--- vo-login-logo.php
+++ vo-login-logo.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://visibleone.com
  * Description: This plugin gives you the ability to modify the logo that appears when you log in to WordPress from a media file, and it also gives you the option to use a remote URL.
  * Author: Conor Visibee <Chen Lay>
- * Version: 1.0.0
+ * Version: 1.1.0
  * Requires at least: 5.6
  * Tested up to: 6.2
  */
@@ -26,22 +26,39 @@ class VO_Login_Logo
         add_filter('login_headerurl', array($this, 'vo_custom_login_logo_url'));
         add_action('admin_menu', array($this, 'vo_login_logo_menu'));
         add_action('admin_init', array($this, 'vo_register_login_logo_settings'));
+        add_action('wp_ajax_vo_upload_logo', array($this, 'vo_upload_logo'));
+        add_action('wp_ajax_nopriv_vo_upload_logo', array($this, 'vo_upload_logo'));
+        add_action('admin_enqueue_scripts', function () {
+            wp_enqueue_media();
+            wp_enqueue_script('jquery');
+        });
     }
+
 
     // Add custom logo to login page
     public function vo_custom_login_logo()
     {
+        $logo_id = get_option('vo_login_logo_id');
         $logo_url = esc_url(get_option('vo_login_logo_url'));
+
+        if ($logo_id && $logo_url) {
+            $logo_src = wp_get_attachment_image_src($logo_id, 'full');
+            $logo_url = $logo_src[0];
+        } elseif (!$logo_id && !$logo_url) {
+            return;
+        }
+
         if (!empty($logo_url) && filter_var($logo_url, FILTER_VALIDATE_URL)) {
             echo '<style type="text/css">
-                #login h1 a{
-                    background-image:url(' . $logo_url . ') !important;
-                    background-size:contain !important;
-                    width:100% !important;
-                }
-            </style>';
+            #login h1 a{
+                background-image:url(' . $logo_url . ') !important;
+                background-size:contain !important;
+                width:100% !important;
+            }
+        </style>';
         }
     }
+
 
     // Add custom logo url to login page
     public function vo_custom_login_logo_url($url)
@@ -65,14 +82,18 @@ class VO_Login_Logo
     {
         register_setting('vo_login_logo_settings_group', 'vo_login_logo_url', 'esc_url_raw');
         register_setting('vo_login_logo_settings_group', 'vo_login_logo_link', 'esc_url_raw');
+        register_setting('vo_login_logo_settings_group', 'vo_login_logo_id', 'intval');
     }
 
     // Create plugin settings page
     public function vo_login_logo_settings_page()
     {
+        $current_version = get_plugin_data(__FILE__)['Version'];
 ?>
         <div class="wrap">
             <h2>VO Login Logo Settings</h2>
+            <p>This plugin was crafted by Conor Visibee &lt;Chen Lay&gt; at <a href="https://visibleone.com/" target="_blank">Visible One</a>.</p>
+            <p>Current version: <?php echo $current_version; ?></p>
             <form method="post" action="options.php">
                 <?php settings_fields('vo_login_logo_settings_group'); ?>
                 <?php do_settings_sections('vo_login_logo_settings_group'); ?>
@@ -85,11 +106,108 @@ class VO_Login_Logo
                         <th scope="row">Logo Link:</th>
                         <td><input type="text" name="vo_login_logo_link" value="<?php echo esc_attr(get_option('vo_login_logo_link')); ?>" /></td>
                     </tr>
+                    <tr valign="top">
+                        <th scope="row">Upload or Select Logo:</th>
+                        <td>
+                            <?php
+                            $logo_id = get_option('vo_login_logo_id');
+                            if ($logo_id) {
+                                $logo_src = wp_get_attachment_image_src($logo_id, 'full');
+                                $logo_url = $logo_src[0];
+                            } else {
+                                $logo_url = '';
+                            }
+                            ?>
+                            <div class="vo-upload-wrap">
+                                <div class="vo-button-wrap">
+                                    <div class="vo-upload-button">
+                                        <button type="button" class="button" id="vo-upload-logo-btn"><?php _e('Upload/Select Image'); ?></button>
+                                    </div>
+                                    <?php if (!empty($logo_url) || !empty(get_option('vo_login_logo_url'))) { ?>
+                                        <div class="vo-remove-button">
+                                            <button type="button" class="vo-remove-logo-btn button"><?php _e('Remove Image'); ?></button>
+                                        </div>
+                                    <?php } ?>
+                                </div>
+                                <div class="vo-preview-image">
+                                    <?php if (!empty($logo_url)) { ?>
+                                        <img src="<?php echo $logo_url; ?>" width="200" />
+                                    <?php } else if (!empty(get_option('vo_login_logo_url'))) { ?>
+                                        <?php $logo_remote_url = esc_html(get_option('vo_login_logo_url')); ?>
+                                        <?php if (filter_var($logo_remote_url, FILTER_VALIDATE_URL)) { ?>
+                                            <img src="<?php echo $logo_remote_url; ?>" width="200" />
+                                        <?php } else { ?>
+                                            <span class="vo-remote-url"><?php echo $logo_remote_url; ?></span>
+                                        <?php } ?>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                            <input type="hidden" name="vo_login_logo_id" id="vo-login-logo-id" value="<?php echo $logo_id; ?>" />
+                        </td>
+                    </tr>
                 </table>
                 <?php submit_button(); ?>
             </form>
         </div>
+
+        <script>
+            jQuery(document).ready(function($) {
+                var vo_upload_frame;
+                $('#vo-upload-logo-btn').on('click', function(e) {
+                    e.preventDefault();
+                    if (vo_upload_frame) {
+                        vo_upload_frame.open();
+                        return;
+                    }
+                    vo_upload_frame = wp.media.frames.file_frame = wp.media({
+                        title: '<?php _e('Upload or Select Image'); ?>',
+                        button: {
+                            text: '<?php _e('Use this image'); ?>'
+                        },
+                        multiple: false
+                    });
+                    vo_upload_frame.on('select', function() {
+                        var attachment = vo_upload_frame.state().get('selection').first().toJSON();
+                        $('.vo-preview-image').html('<img src="' + attachment.url + '" width="200" />');
+                        $('#vo-login-logo-id').val(attachment.id);
+                    });
+                    vo_upload_frame.open();
+                });
+
+                $('.vo-remove-logo-btn').on('click', function(e) {
+                    e.preventDefault();
+                    $('.vo-preview-image').html('');
+                    $('#vo-login-logo-id').val('');
+                });
+            });
+        </script>
+
+        <style>
+            .vo-upload-wrap .vo-button-wrap {
+                display: flex;
+                align-items: center;
+            }
+
+            .vo-upload-wrap .vo-button-wrap .vo-remove-button {
+                margin-left: 10px;
+            }
+
+            .vo-preview-image {
+                margin-top: 1rem;
+            }
+        </style>
+
 <?php
+    }
+
+    // Upload or select logo and save to database
+    public function vo_upload_logo()
+    {
+        check_ajax_referer('vo_upload_logo_nonce', 'security');
+
+        $attachment_id = intval($_POST['attachment_id']);
+        update_option('vo_login_logo_id', $attachment_id);
+        wp_send_json_success();
     }
 }
 
